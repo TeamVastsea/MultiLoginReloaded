a8d408a681493d2a6c702fafc6dbd69fb55a5c2c
zrll <zhengruilin0@126.com>
2026-02-15 01:25:52 +0800

    Fix crash on Velocity #575(Fixes #1)

--- velocity/injector/src/main/java/moe/caa/multilogin/velocity/injector/handler/MultiInitialLoginSessionHandler.java
+++ velocity/injector/src/main/java/moe/caa/multilogin/velocity/injector/handler/MultiInitialLoginSessionHandler.java
@@ -132,7 +132,8 @@ public class MultiInitialLoginSessionHandler {
                         VelocityServer.class,
                         LoginInboundConnection.class,
                         com.velocitypowered.api.util.GameProfile.class,
-                        boolean.class
+                        boolean.class,
+                        String.class
                 )
         ));
     }
@@ -218,13 +219,12 @@ public class MultiInitialLoginSessionHandler {
                                 try {
                                     this.mcConnection.setActiveSessionHandler(StateRegistry.LOGIN,
                                             (AuthSessionHandler) authSessionHandler_allArgsConstructor.invoke(
-                                                    this.server, inbound, generateGameProfile(finalGameProfile), true
+                                    this.server, inbound, generateGameProfile(finalGameProfile), true, serverId
                                             ));
                                 } catch (Throwable e) {
                                     throw new RuntimeException(e);
                                 }
                             }).get();
-
                         } else {
                             this.inbound.disconnect(Component.text(result.getKickMessage()));
                         }
